3c8ddbb3f99e2deba7440c3c25dc08740b01d4d2
Eli Reiners <goelyukim@gmail.com>
2019-11-11 09:25:11 -0500

    update

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 function eliCode() {
-    return `<section class="header index dark">
-<p>Hi, from Eli Reiner</p></section>
-<section class="bio index light">
-<p>bio</p></section>
-<section class="connect index dark">
-<p>Let's solve: developerelireiner@gmail.com</p></section>`
+    return `<section>
+    <p class="blue">About</p>
+    <br>
+    <p>I'm a <span class="bold">web developer</span> studying at Thinkful.
+    I like building solutions with code, enjoy my coffee with vanilla JS, 
+    and fully invest my self in what I do.
+    Mindfulness keeps my operating system humming.</p>
+    <br>
+<p>A while ago, I read an article by an engineering team leader that opened my mind to a new tool for solving problems.
+ That new tool is called software. Since then <span class="bold">solving problems with software captures my drive</span>.  
+ Then I decided to fully immerse myself in the world of software and become a Full Stack Web Developer. 
+ Ever since <span class="bold">I’m looking forward to solving yet another problem</span>.</p>
+          <br>
+    <p>Let's solve: developerelireiner@gmail.com</p>
+</section>`
 }
 
 function renderEli() {
@@ -14,13 +23,24 @@ function renderEli() {
 }
 
 function contactCode() {
-    return `<img src="test-profile.jpg" alt="An image of Eli Reiner">
-<section class="name contact light"><p>Eli Reiner</p>
-</section><section class="number contact dark"><p>Phone: 718-702-4006</p>
-</section><section class="email contact light"><p>Email: developerelireiner@gmail.com</p>
-</section><section class="github-linkedin contact dark">
-<p><a href="https://github.com/elireiner/" target="_blank">GitHub</a>
-<a href="https://www.linkedin.com/in/eli-reiner-09b873161"target="_blank">LinkedIn</a></p></section>`;
+    return `<section class="profile"><img class="profile" src="profile.jpg" alt="An image of Eli Reiner">
+        <p>Eli Reiner</p><p class="small">New York, NY</p></section>
+        <div class="contact">
+    <section class="contact">
+        <p>718-702-4006</p>
+        <p>Phone</p>
+    </section>
+    <hr>
+    <section class="contact">
+        <p>developerelireiner@gmail.com</p>
+        <p>Email</p>
+    </section>
+    <hr>
+    </div>
+    <section class="github-linkedin">
+        <p><a href="https://github.com/elireiner/" target="_blank">GitHub</a></p>
+           <p><a href="https://www.linkedin.com/in/eli-reiner-09b873161" target="_blank">LinkedIn</a></p>
+    </section>`;
 }
 function renderContact() {
     $('nav').on('click', '.contact', function () {
@@ -29,11 +49,19 @@ function renderContact() {
 }
 
 function portfolioCode() {
-    return `<section class="explanation portfolio dark"><p>This app will let you test your knowledge about SpaceX</p>
-</section><section class="links portfolio light"><p><a href="https://elireiner.github.io/quiz-app/" target="_blank">live</a>
-<a href="https://github.com/elireiner/quiz-app" target="_blank">repo</a></p>
-</section><section class="explanation portfolio dark">
-<img src="quiz-app.jpg" alt="screenshot of my quiz-app project"></section>`;
+    return `<div class="portfolio">
+    <div class="quiz-app">
+    <p class="explanation">This app will let you test your knowledge about SpaceX</p>
+    <section class="portfolio-links">
+        <p><a href="https://elireiner.github.io/quiz-app/" target="_blank">live</a></p>
+        <p><a href="https://github.com/elireiner/quiz-app" target="_blank">repo</a></p>
+    </section>
+</div>
+
+<section class="portfolio">
+    <img src="quiz-app.jpg" alt="screenshot of my quiz-app project">
+</section>
+</div>`;
 }
 
 function renderPortfolio() {
